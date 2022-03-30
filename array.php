8797e80f68614db56bd2926b39692d8012b1b053
Sarah Lancaster <Sarah.Lancaster@students.jmcss.org>
2022-03-30 09:17:08 -0500

    kjb;khb

---
+++ array.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+<script src="https://kit.fontawesome.com/3024bfed98.js" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css2?family=Sonsie+One&display=swap" rel="stylesheet">
+    <title>E-commerce</title>
+  </head>
+  <body>
+
+<?php
+
+//String
+$name = 'Coding is fun';
+
+//Integer
+$name = 20;
+
+//Float
+$name = 20.5784;
+
+//Array
+$names = array("Daniel" , "Dennis" , "Michael")
+echo $names ['1'];
+
+?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -14,8 +14,6 @@
   </head>
   <body>
 
-
-
     <!--Nav Bar-->
     <nav class="navbar navbar-expand-sm navbar-dark bg-rgb(95, 81, 172)">
         <div class="container">
@@ -107,7 +105,21 @@
      </section>
     <!--End of Slider-->
 
+<form method = "GET">
+    <input type = "text" name = "person">
+    <button>SUBMIT </button>
+</form>
 
+
+<?php
+    $name = $_GET ['person'];
+    echo $name. " is a handsome fellow!";
+?>
+
+
+<div class="variables">
+
+</div>
 
 
      <!--Service Section-->
@@ -340,5 +352,5 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+    </body>
 </html>
