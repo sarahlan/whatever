de55fa19557021f77d2e7d059df8bf50e8996269
Sarah Lancaster <Sarah.Lancaster@students.jmcss.org>
2022-04-04 21:14:58 -0500

    0eiojsvoir

--- calculator.php
+++ calculator.php
@@ -3,9 +3,9 @@
 ?>
 
  <form>
-<input type = "text" name="num1" placeholder= "Number 1">
-<input type = "text" name="num2" placeholder= "Number 2">
-<select name = "operator">
+<input type ="text" name="num1" placeholder="Number 1">
+<input type ="text" name="num2" placeholder="Number 2">
+<select name ="operator">
     <option>None</option>
     <option>Add</option>
     <option>Subtract</option>
@@ -16,32 +16,30 @@
 <button type = "submit" name = "submit" value = "sumbit"> Calculate </button>
 </br>
 </form>
-<p> The answer is: </p>
-
-
-    <?php
-        if(isset($_GET['submit'])){
+<p> The answer is:</p>
+<?php
+        if(isset($_GET['submit'])) {
             $result1 = $_GET['num1'];
             $result2 = $_GET['num2'];
             $operator = $_GET['operator'];
         }
-        switch($operator){
+        switch($operator) {
             case "None":
                 echo "You need to select a method!";
             break;
             case "Add":
-                echo "$result1 + $result2";
+                echo $result1 + $result2;
             break;
             case "Subtract":
-                echo "$result1 - $result2";
+                echo $result1 - $result2;
             break;
             case "Multiply":
-                echo "$result1 * $result2";
+                echo $result1 * $result2;
             break;
             case "Divide":
-                echo "$result1 / $result2";
+                echo $result1 / $result2;
             break;
-        }
+    }
     ?>
     
 </body>
