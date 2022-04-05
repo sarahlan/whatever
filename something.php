84b39bca70f8919a400e7357d30345a50d9fbc3c
Sarah Lancaster <Sarah.Lancaster@students.jmcss.org>
2022-04-05 09:38:34 -0500

    wqd

--- something.php
+++ something.php
@@ -1,3 +1,14 @@
+<?php
+    include 'header.php';
+?>
+<section>
+          <div class = "someMainContent">
+              <h1>Hi there!</h1>
+</div>
+</section>
+</body>
+</html>
+
 function () {
     
 }
